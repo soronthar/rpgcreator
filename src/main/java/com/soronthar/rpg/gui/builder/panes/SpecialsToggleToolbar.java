2edfbaa62d74d54fa25cace41e8190072d8bf535
soronthar <soronthar>
2012-04-24 03:13:40 -0430

    rpgcreator-4: Create Toggle for Specials - Refactoring actions

--- src/main/java/com/soronthar/rpg/gui/builder/panes/SpecialsToggleToolbar.java
+++ src/main/java/com/soronthar/rpg/gui/builder/panes/SpecialsToggleToolbar.java
@@ -8,26 +8,34 @@ import java.awt.event.ActionEvent;
 public class SpecialsToggleToolbar extends JToolBar {
 
 
-    private final JToggleButton[] toggles = new JToggleButton[3];
+    private final JToggleButton[] toggles;
 
 
     public SpecialsToggleToolbar(final ActionsManager actions) {
-        toggles[0] = new JToggleButton(new MyAction(actions.addObstacleAction(), 0));
-        toggles[1] = new JToggleButton(new MyAction(actions.addHeroStartAction(), 1));
-        toggles[2] = new JToggleButton(new MyAction(actions.addJumpAction(), 2));
+
+        toggles = new JToggleButton[]{
+                new MyToggleButton(actions.addObstacleAction()),
+                new MyToggleButton(actions.addHeroStartAction()),
+                new MyToggleButton(actions.addJumpAction()),
+
+        };
 
         for (JToggleButton toggle : toggles) {
             this.add(toggle);
         }
     }
 
+    class MyToggleButton extends JToggleButton {
+        MyToggleButton(Action a) {
+            super(new MyAction(a));
+        }
+    }
+
     class MyAction extends AbstractAction {
-        private int index;
         private Action action;
 
-        MyAction(final Action a, int index) {
+        MyAction(final Action a) {
             super((String) a.getValue(Action.NAME), (Icon) a.getValue(Action.SMALL_ICON));
-            this.index = index;
             this.action = a;
         }
 
@@ -36,9 +44,9 @@ public class SpecialsToggleToolbar extends JToolBar {
             action.actionPerformed(e);
             JToggleButton source = (JToggleButton) e.getSource();
             if (source.isSelected()) {
-                for (int i = 0; i < toggles.length; i++) {
-                    if (i != index) {
-                        toggles[i].setSelected(false);
+                for (JToggleButton toggle : toggles) {
+                    if (!source.equals(toggle)) {
+                        toggle.setSelected(false);
                     }
                 }
             }
